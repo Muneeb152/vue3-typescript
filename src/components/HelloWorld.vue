2e27d093db7ec98d27f81d126279e56745b448fc
muneeb_akhtar <muneeb@superfuture.world>
2023-05-30 17:21:04 +0400

    Intial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,86 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>{{ pow(2, 3) }}</div>
+    <button @click="increment">{{ count }}</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, onErrorCaptured, onMounted, reactive, ref, watch } from 'vue';
+
+interface State {
+  title: string,
+  year: number,
+  todos: Todo[]
+}
+
+interface Todo {
+  name: string,
+  isComplete: boolean
+}
 
 export default defineComponent({
   name: 'HelloWorld',
   props: {
     msg: String,
   },
+  setup(props) {
+    const count = ref(0)
+    const todoId = ref(4)
+    console.log(props);
+    const state = reactive<State>({
+      title: "",
+      year: 2020,
+      todos: [
+        {
+          name: 'Finish ScreenCast',
+          isComplete: false
+        },
+        {
+          name: 'Go to Store',
+          isComplete: false
+        }
+      ]
+    })
+
+    function increment(): void {
+      count.value++;
+      count.value = pow(2, 3);
+    }
+
+    function pow(x: number, y: number): number {
+      return Math.pow(x, y);
+    }
+    const itemsLeft = computed(() => state.todos.filter(todo => !todo.isComplete).length);
+
+    onMounted(() => {
+      console.log("Todo Mounted");
+    })
+
+    watch(() => count.value,
+      (newValue, oldValue) => {
+        console.log('New Value:' + newValue);
+        console.log('Old Value:' + oldValue)
+      }
+    )
+
+    onErrorCaptured(e => {
+      console.log("Error is:" + e)
+    })
+
+
+
+    return {
+      count,
+      increment,
+      pow,
+      state,
+      itemsLeft,
+      todoId
+    }
+
+  }
 });
 </script>
 
@@ -49,14 +89,17 @@ export default defineComponent({
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
